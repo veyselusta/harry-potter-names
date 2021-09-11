2d274c4bce2f8a0d7bcb38571f4d00fadcc53562
veyselusta <vyslusta1@gmail.com>
2021-09-11 19:57:10 +0300

    Create unit tests and separate them to the another file

---
+++ tests/index.test.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const expect = require('chai').expect
+const harryPotterNames = require('../src')
+
+describe('harry-potter-names', function () {
+  it ('should have a list of all available names', function () {
+    expect(isArrayOfString(harryPotterNames.all)).to.be.true
+  })
+
+  it('should allow me to get a random name from the list', function () {
+    expect(isIncludedIn(harryPotterNames.random())(harryPotterNames.all)).to.be.true
+  })
+
+})
+
+function isArrayOfString (array) {
+  return array.every(function (item) {
+    return typeof item === 'string'
+  })
+}
+
+function isIncludedIn (array) {
+  return function (item) {
+    return array.indexOf(item) !== 1
+  }
+}
