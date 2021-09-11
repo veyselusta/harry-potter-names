6d04c6f388f83442358318473bb3217c1826466f
veyselusta <vyslusta1@gmail.com>
2021-09-11 13:07:53 +0300

    Create the index.js and JSON file to create the library

---
+++ src/index.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const uniqueRandomArray =  require('unique-random-array')
+const harryPotterNames = require('./harry-potter-names.json')
+
+const getRandomItem = uniqueRandomArray(harryPotterNames)
+
+module.exports = {
+  all: harryPotterNames,
+  random,
+  sameFirstLetter
+};
+
+function random (number) {
+  if (number === undefined) {
+    return getRandomItem()
+  } else {
+    return Array.from({length: 5}, (v, i) => i).map(number => getRandomItem())
+  }
+}
+
+function sameFirstLetter (char) {
+  return harryPotterNames.map(name => name == char)
+}
